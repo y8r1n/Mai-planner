bfa7ffa5dd5770b6d865630796ed7c35b7d5349d
y8r1n <yesrin14@gmail.com>
2025-11-15 04:39:25 +0900

    Fix:firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,12 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyDHFzJYsEwhpmpv83apXaOb9e4qE1u-zk0",
-  authDomain: "mai-planner.firebaseapp.com",
-  projectId: "mai-planner",
-  storageBucket: "mai-planner.firebasestorage.app",
-  messagingSenderId: "995353408662",
-  appId: "1:995353408662:web:d555fddf6984e38d613eb7",
-  measurementId: "G-RKGC26KVT7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
