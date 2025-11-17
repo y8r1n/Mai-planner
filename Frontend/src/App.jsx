e955c680a5d38e027439a947a962ad2ba6472ce8
y8r1n <yesrin14@gmail.com>
2025-11-18 01:53:05 +0900

    fix: location

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import TodoTab from "./components/TodoTab";
-import Calendar from "./components/Calendar";
-import School from "./components/School";
-import ImageDiary from "./components/ImageDiary";
-import Study from "./components/Study";
-import Withai from "./components/Withai";
-import Subject from "./components/Subject";
-import MentorChat from "./components/MentorChat";
-import QuizAI from "./components/QuizAI";
-import ReviewDetail from "./components/Reviewdetail";
+import NavBar from "./components/NavBar.jsx";
+import Home from "./components/Home.jsx";
+import TodoTab from "./components/TodoTab.jsx";
+import Calendar from "./components/Calendar.jsx";
+import School from "./components/School.jsx";
+import ImageDiary from "./components/ImageDiary.jsx";
+import Study from "./components/Study.jsx";
+import Withai from "./components/Withai.jsx";
+import Subject from "./components/Subject.jsx";
+import MentorChat from "./components/MentorChat.jsx";
+import QuizAI from "./components/QuizAI.jsx";
+import ReviewDetail from "./components/Reviewdetail.jsx";
 import { NotificationProvider } from "./contexts/NotificationContext.jsx";
-import Alarm from "./components/Alarm"; 
+import Alarm from "./components/Alarm.jsx"; 
 import ClearNotifications from "./components/ClearNotifications"; 
-import { AppProvider } from "./contexts/AppContext";
+import { AppProvider } from "./contexts/AppContext .jsx";
 import LoadingSpinner from "./common/LoadingSpinner.jsx";
-import ErrorMessage from "./common/ErrorMessage";
-import Toast from "./common/Toast";
+import ErrorMessage from "./common/ErrorMessage.jsx";
+import Toast from "./common/Toast.jsx";
 
 function App() {
   return (
