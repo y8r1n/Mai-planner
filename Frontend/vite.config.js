6e77702627adf24f4cbeaf271872f1af3998bde1
y8r1n <yesrin14@gmail.com>
2025-11-17 21:30:28 +0900

    fix: config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,14 +8,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  root: "",
+  build: {
+    outDir: "dist",
   },
 });
+
 
 
 
