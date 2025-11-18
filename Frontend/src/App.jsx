529b2481a9af473986c377a775233dfd7fee95cb
y8r1n <yesrin14@gmail.com>
2025-11-18 15:01:29 +0900

    Fix: layout.nav

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,9 @@
-// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 
-// 📌 페이지 컴포넌트
-import NavBar from "./components/NavBar.jsx";
+// 공통 레이아웃
+import Layout from "./components/Layout.jsx";
+
+// 페이지들
 import Home from "./components/Home.jsx";
 import TodoTab from "./components/TodoTab.jsx";
 import Calendar from "./components/Calendar.jsx";
@@ -17,11 +18,11 @@ import ReviewDetail from "./components/Reviewdetail.jsx";
 import Alarm from "./components/Alarm.jsx";
 import ClearNotifications from "./components/ClearNotifications.jsx";
 
-// 📌 전역 컨텍스트
+// 전역 컨텍스트
 import { NotificationProvider } from "./contexts/NotificationContext.jsx";
 import { AppProvider } from "./contexts/AppContext.jsx";
 
-// 📌 전역 UI 컴포넌트
+// 전역 UI
 import LoadingSpinner from "./common/LoadingSpinner.jsx";
 import ErrorMessage from "./common/ErrorMessage.jsx";
 import Toast from "./common/Toast.jsx";
@@ -30,36 +31,37 @@ function App() {
   return (
     <AppProvider>
       <NotificationProvider>
-        {/* 🔥 전역 UI 컴포넌트 */}
+
         <LoadingSpinner />
         <ErrorMessage />
         <Toast />
 
-        {/* 🔥 공통 네비게이션 */}
-        <NavBar />
-
-        {/* 🔥 라우트 */}
         <Routes>
-          <Route path="/" element={<Home />} />
 
-          {/* 기본 페이지 */}
-          <Route path="/todotab" element={<TodoTab />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/school" element={<School />} />
-          <Route path="/withai" element={<Withai />} />
-          <Route path="/study" element={<Study />} />
-          <Route path="/imagediary" element={<ImageDiary />} />
+          {/* 공통 Layout 적용 */}
+          <Route element={<Layout />}>
+            {/* 기본 페이지 */}
+            <Route path="/" element={<Home />} />
+            <Route path="/todotab" element={<TodoTab />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/school" element={<School />} />
+            <Route path="/withai" element={<Withai />} />
+            <Route path="/study" element={<Study />} />
+            <Route path="/imagediary" element={<ImageDiary />} />
 
-          {/* 과목 상세 & 주차 상세 */}
-          <Route path="/subject/:id" element={<Subject />} />
-          <Route path="/mentorchat/:subjectId/:weekId" element={<MentorChat />} />
-          <Route path="/quizai/:subjectId/:weekId" element={<QuizAI />} />
-          <Route path="/reviewdetail/:subjectId/:weekId/:noteId" element={<ReviewDetail />} />
+            {/* 상세 페이지 */}
+            <Route path="/subject/:id" element={<Subject />} />
+            <Route path="/mentorchat/:subjectId/:weekId" element={<MentorChat />} />
+            <Route path="/quizai/:subjectId/:weekId" element={<QuizAI />} />
+            <Route path="/reviewdetail/:subjectId/:weekId/:noteId" element={<ReviewDetail />} />
 
-          {/* 알림 */}
-          <Route path="/alarm" element={<Alarm />} />
-          <Route path="/clear-notifications" element={<ClearNotifications />} />
+            {/* 알림 */}
+            <Route path="/alarm" element={<Alarm />} />
+            <Route path="/clear-notifications" element={<ClearNotifications />} />
+          </Route>
+
         </Routes>
+
       </NotificationProvider>
     </AppProvider>
   );
